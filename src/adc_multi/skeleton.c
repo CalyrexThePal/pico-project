15f16690b5bf236aacff8ea77c7699ec363f1c6e
CalyrexThePal <2801789sss@gmail.com>
2024-07-10 16:46:03 -0700

    change some of the junky features for previous version, now make sure sender and receiver pin doesn't rely on a same channel so that the program is more readable

--- src/adc_multi/skeleton.c
+++ src/adc_multi/skeleton.c
@@ -3,31 +3,32 @@
 #include "hardware/adc.h"
 #include "hardware/timer.h"
 
+#define SENDER_PIN 4 // GPIO-4 - pin used to send flag signal for unstalling main
+#define RECEIVER_PIN 5 // GPIO-5 - pin used to receive flag signal for unstalling main
+
+// ADC unit config
+#define ADC_PULSE_PIN 2 // GPIO-2 - pin pulled to receive pulse signal for ADC read
 #define ADC_PIN 26 // ADC0 aka GPIO-26, pin corresponding to adc unit
-#define STALl_FLUSH_PIN 4 // GPIO-4 - pin used to send/receive stalling singal
-#define TRIGGER_PIN 2 // GPIO-2 - pin used to receive pulse signal
 #define ADC_CHANNEL 0 // ADC channels, pick from 0-3 (4 is reserved for temp. sensor)
 
-#define SAMPLE_BUFFER_SIZE 32768 // choose your buffer size (logrithmic of 2), 2^(x=15)
+// choose your buffer size (power of 2), 2^(x=15) to avoid page fault
+#define SAMPLE_BUFFER_SIZE 32768 
 #define BUFFER_THRESHOLD 22222
 
 #define Fs 50000.0 // Sample rate (Hz) (must not goes higer than 75 kSPS)
 #define ADCCLK 48000000.0 // ADC clock rate (unmutable!)
 
-#define SENDING 0
-#define RECEIVING 1
-
 // ---------------- preprocessor variable ----------------
-// #define DEEBUGG
 #define PRINT_BUFFER
+#define RECORD_TIME
 
 volatile bool lock;
-volatile bool program_status;
-
 volatile uint16_t sample_buffer[SAMPLE_BUFFER_SIZE];
 volatile uint32_t timestamp[SAMPLE_BUFFER_SIZE];
 volatile uint16_t sample_index = 0;
 volatile bool sampling_done = false;
+
+unsigned short machine_enum = 1;
 
 // digital-to-voltage conversion
 const float conversion_factor = 3.3f / (1 << 12); 
@@ -36,7 +37,6 @@ const float conversion_factor = 3.3f / (1 << 12);
 
 /*
     Callback function to unlock the stalling flag before reading
-
 */
 void unlock_trigger_callback(uint gpio, uint32_t events) {
     lock = false;
@@ -53,21 +53,24 @@ void unlock_trigger_callback(uint gpio, uint32_t events) {
     Return:
         NULL
 */
-void trigger_callback(uint gpio, uint32_t events) {
+void ADC_trigger_callback(uint gpio, uint32_t events) {
     if (sample_index < SAMPLE_BUFFER_SIZE) {
         // single ADC sample acquire
         sample_buffer[sample_index] = adc_read();
-        // get timestamp in microsecond
-        timestamp[sample_index] = time_us_32();
 
+#ifdef RECORD_TIME
+        timestamp[sample_index] = time_us_32(); // get timestamp in microsecond
+#endif
         sample_index++;
 
         // check if the index exceeds certain threshold
         if (sampel_index == BUFFER_THRESHOLD){
-            gpio_set_dir(STALl_FLUSH_PIN, GPIO_OUT);
-            gpio_pull_up(STALl_FLUSH_PIN);
+            // set out-mode for sender pin, and pull up for irq sending
+            gpio_set_dir(SENDER_PIN, GPIO_OUT);
+            gpio_pull_up(SENDER_PIN);
 
-            // TODO: implement sending signal logic to another pico
+            // TODO: implement signal sending logic to machine 2
+
         }
 
         // check if the sample index is out of the BUFFER bound
@@ -90,26 +93,34 @@ void print_buffer(){
     // output the buffer over serial
     for (int i = 0; i < SAMPLE_BUFFER_SIZE; i++) {
         uint16_t result = sample_buffer[i];
+#ifdef RECORD_TIME
         uint32_t time = timestamp[i];
         printf("Raw value: %d, voltage: %f, at time: %d\n", result, result * conversion_factor, time);
+#elif
+        printf("Raw value: %d, voltage: %f", result, result * conversion_factor);
+#endif
     }
 }
 
 int main() {
 
     lock = false;
-    program_status = SENDING;
 
     stdio_init_all(); // initialize stdio lib
     printf("Program started\n");
 
+    // initialize all the operating pin
     adc_init();
-    gpio_init(TRIGGER_PIN);
-    gpio_init(STALl_FLUSH_PIN);
+    gpio_init(ADC_PULSE_PIN);
+    gpio_init(RECEIVER_PIN);
+    gpio_init(SENDER_PIN);
+    printf("Operating PIN's initialized\n");
 
-    // ----------------------------------------
-    // -------------- ADC Config --------------
-    // ----------------------------------------
+    /*
+        ADC configs:
+            - set Analog channel
+            - set sampling rate
+    */
     adc_gpio_init(ADC_PIN);
     adc_select_input(ADC_CHANNEL);
     adc_set_clkdiv(ADCCLK/Fs); // adjust the sampling rate
@@ -117,32 +128,42 @@ int main() {
 
 lableStall:
 
-    // --------------------------------------------
-    // ----- Communication PIN for Stalling -------
-    // --------------------------------------------
-    if (program_status == RECEIVING) {
-        gpio_set_dir(STALl_FLUSH_PIN, GPIO_IN);
-        gpio_pull_up(STALl_FLUSH_PIN);
+    // *************************************************
+    // ------------- Stalling Stage Exited -------------
+    // -------------------------------------------------
+    if (machine_enum <= 1) {
+        // set in-mode for receiver pin and pull it up for irq pending
+        gpio_set_dir(RECEIVER_PIN, GPIO_IN);
+        gpio_pull_up(RECEIVER_PIN);
 
-        // initialize interrupt call back for stalling
-        gpio_set_irq_enabled_with_callback(STALl_FLUSH_PIN, GPIO_IRQ_EDGE_RISE, true, &unlock_trigger_callback);
+        // initialize callback function for stalling stage, unlock once interrupted 
+        gpio_set_irq_enabled_with_callback(RECEIVER_PIN, GPIO_IRQ_EDGE_RISE, true, &unlock_trigger_callback);
 
         while(lock){
             tight_loop_contents();
         }
     }
+    // -------------------------------------------------
+    // ------------- Stalling Stage Exited -------------
+    // *************************************************
 
-    // ----------------------------------------
-    // ----- Triggering PIN for Reading -------
-    // ----------------------------------------
-    gpio_set_dir(TRIGGER_PIN, GPIO_IN);
-    gpio_pull_up(TRIGGER_PIN);
-    gpio_set_irq_enabled_with_callback(TRIGGER_PIN, GPIO_IRQ_EDGE_RISE, true, &trigger_callback);
-    printf("Trigger pin initialized\n");
+
+    // *************************************************
+    // ---------------- ADC Read Starts ----------------
+    // -------------------------------------------------
+    gpio_set_dir(ADC_PULSE_PIN, GPIO_IN);
+    gpio_pull_up(ADC_PULSE_PIN);
+    gpio_set_irq_enabled_with_callback(ADC_PULSE_PIN, GPIO_IRQ_EDGE_RISE, true, &ADC_trigger_callback);
+    printf("ADC pulse pin initialized\n");
 
     while (!sampling_done) {
         tight_loop_contents();
     }
+    // -------------------------------------------------
+    // --------------- ADC Read Complete ---------------
+    // *************************************************
+
+
 
     // TODO: implement buffer transferring interface
     if(transfer_buffer()){
@@ -155,16 +176,9 @@ lableStall:
         printf("Error: Cannot be clear");
         return 1;
     }    
-
-    // TODO: Altered the STATUS
-    if (program_status == RECEIVING){
-        program_status = SENDING;
-        lock = false;
-    }else{
-        program_status = RECEIVING;
-        lock = true;
-    }
     
+    machine_enum++;
+    lock = true;
     sample_index = 0;
 
 goto labelStall;
